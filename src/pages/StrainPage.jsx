d983e22b755007551509155839c8add4c088bf13
nunned <chanonac@outlook.com>
2023-11-10 17:27:40 -0600

    strain page for testing

--- src/pages/StrainPage.jsx
+++ src/pages/StrainPage.jsx
@@ -1,13 +1,42 @@
 // StrainPage.jsx
+import { useState, useEffect } from 'react';
 
 const StrainPage = () => {
-    return (
-      <div>
-        <h1>Strain Page</h1>
-        {/* Add content for the Strain Page here */}
-      </div>
-    );
-  };
-  
-  export default StrainPage;
-  
+  const [plants, setPlants] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Replace 'http://example.com/api/plants' with the actual endpoint of your Django backend
+    fetch('http://example.com/api/plants')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setPlants(data); // Assuming the backend returns an array of plant names
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, []); // The empty array as a second argument ensures the effect runs only once after the initial render
+
+  return (
+    <div>
+      <h1>Strain Page</h1>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {error}</div>}
+      <ul>
+        {plants.map((plant, index) => (
+          <li key={index}>{plant.name}</li> // Replace 'plant.name' with the actual property that holds the name in your data
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default StrainPage;

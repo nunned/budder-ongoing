e3dc465b1885e245f0439e1efb0c0f81dcbd4197
nunned <chanonac@outlook.com>
2023-12-07 16:21:41 -0600

    updates to dashplants page and dashcarousel

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 # TODO
 - make carousel card width dynamic.
+- connect icons to quick actions menu
--- src/pages/dash-pages/DashPlants.jsx
+++ src/pages/dash-pages/DashPlants.jsx
@@ -2,8 +2,53 @@ import "./dashpage-styles.css";
 import DashCard from "../../components/dash-comps/DashCard";
 import LiveImage from "../../assets/LiveImage.svg";
 import QuickActions from "../../components/dash-comps/QuickActions";
+import DashCarousel from "../../components/Carousel/DashCarousel";
 
 function DashPlants() {
+  // Sample data for rooms
+  const roomData = [
+    <div key="room1" className="room-item">
+      Room 1
+    </div>,
+    <div key="room2" className="room-item">
+      Room 2
+    </div>,
+    <div key="room3" className="room-item">
+      Room 3
+    </div>,
+    // ... additional rooms
+  ];
+
+  const plantItems = [
+    {
+      action: "Changed Growth Phase",
+      user: "User1",
+      timeAgo: "1m ago",
+    },
+    {
+      action: "Destroyed",
+      user: "User2",
+      timeAgo: "2m ago",
+    },
+    {
+      action: "Created",
+      user: "User3",
+      timeAgo: "3m ago",
+    },
+    {
+      action: "Moved",
+      user: "User4",
+      timeAgo: "4m ago",
+    },
+    // ... more items
+  ].map((item, index) => (
+    <div key={index} className="dash-item">
+      <div className="dash-item-action">{item.action}</div>
+      <div className="dash-item-user">{item.user}</div>
+      <div className="dash-item-time">{item.timeAgo}</div>
+    </div>
+  ));
+
   return (
     <div className="dash-plants-wrap">
       <h1 className="dash-plants-header">
@@ -11,12 +56,15 @@ function DashPlants() {
         <img src={LiveImage} alt="Live" className="live-icon" />
       </h1>
       <DashCard
-        className="dash-plants-card" // Add this line
+        className="dash-plants-card"
         label="ACTIVE PLANTS"
         value="2,690"
         descriptor="TOTAL"
       />
       <QuickActions />
+      <DashCarousel headerTitle="Rooms" dashItems={roomData} />
+      {/* stages */}
+      <DashCarousel headerTitle="Plants History" dashItems={plantItems} />
     </div>
   );
 }
